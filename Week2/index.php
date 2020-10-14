13bfe67f99b2626cd4d6ec6ac7e37dd3de841e40
Joshua Moron <joshua.moron@burohappold.com>
2020-10-14 14:51:34 +0100

    Updated Styling

--- Week2/index.php
+++ Week2/index.php
@@ -1,18 +1,47 @@
-<h1>DEEETS</h1>
 
-
-<form method="POST" action="insert.php">
-    <input type="email" name="email" required placeholder="E-mail">
-    <br>
-    <input type="password" name="password" required placeholder="Password">
-    <br>
-    <input type="text" name="fname" required placeholder="First Name">
-    <br>
-    <input type="text" name="lname" required placeholder="Last Name">
-    <br>
-    <button type="submit">ADD</button>
-
-</form>
+<style>
+    .form {
+        display: flex;
+        border: dotted;
+        margin: auto;
+        width: 200px;
+        border: 3px solid green;
+        padding: 10px;
+        text-align: center;
+    }
+    .table {
+        margin: auto;
+        width: 50%;
+        padding: 10px;
+        text-align: center;
+        border:solid;
+    }
+    .input{
+        border:solid;
+        text-align: left;
+    }
+    .title{
+        color: red;
+        text-align: center;
+        font-size: 5em;
+        font-family: "Times New Roman", Times, serif;
+        font-style:oblique;
+    }
+</style>
+<h1 class="title">DEEETS</h1>
+<div class="form">
+    <form method="POST" action="insert.php">
+        <input class="input" type="email" name="email" required placeholder="E-mail">
+        <br>
+        <input class="input"type="password" name="password" required placeholder="Password">
+        <br>
+        <input class="input"type="text" name="fname" required placeholder="First Name">
+        <br>
+        <input class="input"type="text" name="lname" required placeholder="Last Name">
+        <br>
+        <button type="submit">ADD</button>
+    </form>
+</div>
 
 <?php
 //connects to db
@@ -28,15 +57,15 @@ $run = mysqli_query($db_connect, $sql);
 
 
 
-<table border="100">
+<table class="table" border>
     <thead>
         <tr>
-            <th>Email</th>
-            <th>Password</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <th class="table">Email</th>
+            <th class="table">Password</th>
+            <th class="table">First Name</th>
+            <th class="table">Last Name</th>
+            <th class="table">Edit</th>
+            <th class="table">Delete</th>
         </tr>
     </thead>
     <tbody>
@@ -49,8 +78,8 @@ $run = mysqli_query($db_connect, $sql);
                     ?></td>
                 <td><?= $result["fname"] ?></td>
                 <td><?= $result["lname"] ?></td>
-                <td><a href="edit.php?e=<?=$result["UID"]?>">EDIT</a></td>
-                <td><a href="delete.php?d=<?=$result["UID"]?>">Delete</a></td>
+                <td><a href="edit.php?e=<?= $result["UID"] ?>">EDIT</a></td>
+                <td><a href="delete.php?d=<?= $result["UID"] ?>">Delete</a></td>
             </tr>
         <?php }; ?>
 
---
+++ Week2/login.php
@@ -0,0 +1,10 @@
+<h2>LOGIN to the Program</h2>
+<form method ="POST" action= "_auth.php">
+<input type="email" name="email" required placeholder="E-mail">
+        <br>
+        <input type="password" name="password" required placeholder="Password">
+        <br>
+        <button type="submit">LOGIN</button>
+
+
+</form>
